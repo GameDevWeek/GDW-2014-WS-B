855da9f861007e5991f3800707a3e8c201780d39
unknown <rftpool22@rft2209.rz.fh-trier.de>
2015-03-17 16:57:25 +0100

    speichern

---
+++ gdx-labyrinth/src/main/java/de/hochschuletrier/gdw/ws1415/game/input/Input_Puffer.java
@@ -0,0 +1,13 @@
+package de.hochschuletrier.gdw.ws1415.game.input;
+
+import java.util.HashMap;
+import com.badlogic.gdx.math.Vektor2;
+
+public class Input_Puffer {
+    
+    public enum Input {
+        click
+    }
+    
+    HashMap<Input,Vector2D> puffer;
+}
