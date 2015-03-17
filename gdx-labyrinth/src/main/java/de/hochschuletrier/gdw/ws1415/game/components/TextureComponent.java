ff7d462dba105c92b5294622f7a18d0bda7a7145
David <davidsiepen95@gmail.com>
2015-03-17 14:13:50 +0100

    Ein Component, der die Textur als String (für json) speichert.

---
+++ gdx-labyrinth/src/main/java/de/hochschuletrier/gdw/ws1415/game/components/TextureComponent.java
@@ -0,0 +1,9 @@
+package de.hochschuletrier.gdw.ws1415.game.components;
+
+import com.badlogic.ashley.core.Component;
+
+public class TextureComponent extends Component {
+
+	public String texture;
+
+}
