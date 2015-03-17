c5a31d186fec8047aa09a2024fafcece5186e326
David <davidsiepen95@gmail.com>
2015-03-17 14:21:09 +0100

    Habe die reset()-Methode geschrieben

--- gdx-labyrinth/src/main/java/de/hochschuletrier/gdw/ws1415/game/components/TileComponent.java
+++ gdx-labyrinth/src/main/java/de/hochschuletrier/gdw/ws1415/game/components/TileComponent.java
@@ -2,9 +2,15 @@ package de.hochschuletrier.gdw.ws1415.game.components;
 
 import com.badlogic.ashley.core.Component;
 import com.badlogic.gdx.utils.Pool;
+import com.badlogic.gdx.utils.Pool.Poolable;
 
-public class TileComponent extends Component {
+public class TileComponent extends Component implements Poolable {
 
 	TileType tileType;
+
+	@Override
+	public void reset() {
+		this.tileType = null;
+	}
 
 }
