e346d434237663f168d8c728dcaf438652fda4f5
David <davidsiepen95@gmail.com>
2015-03-17 16:17:30 +0100

    unnecessary imports gone

--- gdx-labyrinth/src/main/java/de/hochschuletrier/gdw/ws1415/game/Game.java
+++ gdx-labyrinth/src/main/java/de/hochschuletrier/gdw/ws1415/game/Game.java
@@ -2,40 +2,13 @@ package de.hochschuletrier.gdw.ws1415.game;
 
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.ashley.core.PooledEngine;
-import com.badlogic.gdx.Input;
-import com.badlogic.gdx.InputAdapter;
-import com.badlogic.gdx.audio.Sound;
-import com.badlogic.gdx.physics.box2d.Body;
-import com.badlogic.gdx.physics.box2d.BodyDef;
-import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
 
-import de.hochschuletrier.gdw.commons.devcon.cvar.CVarBool;
-import de.hochschuletrier.gdw.commons.gdx.assets.AnimationExtended;
 import de.hochschuletrier.gdw.commons.gdx.assets.AssetManagerX;
-import de.hochschuletrier.gdw.commons.gdx.input.hotkey.Hotkey;
-import de.hochschuletrier.gdw.commons.gdx.input.hotkey.HotkeyModifier;
-import de.hochschuletrier.gdw.commons.gdx.physix.PhysixBodyDef;
-import de.hochschuletrier.gdw.commons.gdx.physix.PhysixComponentAwareContactListener;
-import de.hochschuletrier.gdw.commons.gdx.physix.PhysixFixtureDef;
-import de.hochschuletrier.gdw.commons.gdx.physix.components.PhysixBodyComponent;
-import de.hochschuletrier.gdw.commons.gdx.physix.components.PhysixModifierComponent;
-import de.hochschuletrier.gdw.commons.gdx.physix.systems.PhysixDebugRenderSystem;
-import de.hochschuletrier.gdw.commons.gdx.physix.systems.PhysixSystem;
 import de.hochschuletrier.gdw.ws1415.Main;
-import de.hochschuletrier.gdw.ws1415.game.components.AnimationComponent;
-import de.hochschuletrier.gdw.ws1415.game.components.ImpactSoundComponent;
 import de.hochschuletrier.gdw.ws1415.game.components.PositionComponent;
 import de.hochschuletrier.gdw.ws1415.game.components.TextureComponent;
 import de.hochschuletrier.gdw.ws1415.game.components.TileComponent;
-import de.hochschuletrier.gdw.ws1415.game.components.TriggerComponent;
-import de.hochschuletrier.gdw.ws1415.game.contactlisteners.ImpactSoundListener;
-import de.hochschuletrier.gdw.ws1415.game.contactlisteners.TriggerListener;
-import de.hochschuletrier.gdw.ws1415.game.systems.AnimationRenderSystem;
 import de.hochschuletrier.gdw.ws1415.game.systems.RenderingSystem;
-import de.hochschuletrier.gdw.ws1415.game.systems.UpdatePositionSystem;
-import de.hochschuletrier.gdw.ws1415.game.utils.PhysixUtil;
-
-import java.util.function.Consumer;
 
 public class Game {
 
@@ -48,7 +21,6 @@ public class Game {
 	private final RenderingSystem renderingSystem = new RenderingSystem(
 			GameConstants.PRIORITY_RENDERING);
 
-
 	public Game() {
 
 	}
@@ -58,9 +30,9 @@ public class Game {
 	}
 
 	public void init(AssetManagerX assetManager) {
-		
+
 		addSystems();
-		
+
 		createTile(assetManager);
 	}
 
@@ -84,7 +56,7 @@ public class Game {
 
 		entity.getComponent(PositionComponent.class).x = 500f;
 		entity.getComponent(PositionComponent.class).y = 500f;
-		
+
 		engine.addEntity(entity);
 	}
 }
