8f86502d8650367959a0ef48987a209bc03896fb
David <davidsiepen95@gmail.com>
2015-03-20 15:00:00 +0100

    GameBoardInformation fix

--- gdx-labyrinth/src/main/java/de/hochschuletrier/gdw/ws1415/game/utils/GameBoardInformation.java
+++ gdx-labyrinth/src/main/java/de/hochschuletrier/gdw/ws1415/game/utils/GameBoardInformation.java
@@ -28,28 +28,6 @@ public class GameBoardInformation {
 	// TITLE-FIELD IN PIXLE !!
 	final public static float TILE_FIELD = TILE_SIZE * NUMBER_OF_TILE;
 
-=======
-    
-    // WIDTH from the game, total of 1 !!
-    /** width of the menu in percent to the total screen of the game */
-    final public static float GAME_MENU_WIDTH = 0.28f;  
-    /** width of the menu in percent to the total screen of the game */
-    final public static float GAME_SCREEN_WIDTH = 3/4f;
-    
-    
-    // HEIGTH from the game, total of 1 !!
-    final public static float GAME_NEXT_TILE = 0.32f;
-    final public static float GAME_PLAYER_INFORMATION = 0.68f;
-    
-    // ARROWS WIDTH IN PIXLE
-    public static int ARROWS_WIDTH = 0; // 2x !!!!!
-    public static int ARROWS_HEIGHT = 0;
-    
-    // TITLE-FIELD IN PIXLE !!
-    final public static int TILDE_FIELD = 700;
-    
-    final public static float NUMBER_OF_TILE = 7;
->>>>>>> Temporary merge branch 2
 	// TileMovementSpeed
 	public final static float MOVEMENT_SPEED = 5f;
 
