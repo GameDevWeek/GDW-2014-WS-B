a156b22b82a29fa6d629612251fe88f4d4b7b067
Martin Keren <martin_keren@gmx.de>
2015-03-22 11:50:20 +0100

    WinCondition

---
+++ gdx-labyrinth/src/main/java/de/hochschuletrier/gdw/ws1415/game/components/GoalComponent.java
@@ -0,0 +1,18 @@
+package de.hochschuletrier.gdw.ws1415.game.components;
+
+import com.badlogic.ashley.core.Component;
+import com.badlogic.gdx.utils.Pool;
+
+public class GoalComponent extends Component implements Pool.Poolable {
+
+   public boolean spaten_is_real = true;
+   public boolean isReached = false;
+
+    @Override
+    public void reset() {
+    	isReached = false;
+    	spaten_is_real = false;
+    }
+}
+
+
